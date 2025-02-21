176c63248f3f12d4ee99559b353f65ed1ae07052
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2025-02-21 00:12:15 -0300

    feat: add ranking routes

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,10 @@ import fastifySwaggerUi from '@fastify/swagger-ui'
 import { subscribeToEventRoute } from './routes/subscribe-to-event'
 import { env } from './env'
 import { accessInviteLinkRoute } from './routes/access-invite-link'
+import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites.count'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position'
+import { getRankingRoute } from './routes/get-ranking'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -37,6 +41,10 @@ app.setValidatorCompiler(validatorCompiler)
 
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
+app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberRankingPositionRoute)
+app.register(getRankingRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running')
